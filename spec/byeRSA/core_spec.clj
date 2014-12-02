145e12dceb2a4916f1d33a3a8bb449a48be7b9a9
Kristina <kristina.lyubomirova@bbva.com>
2014-12-02 20:14:49 +0100

    added is-prime method

--- spec/byeRSA/core_spec.clj
+++ spec/byeRSA/core_spec.clj
@@ -5,4 +5,12 @@
 
 (describe "In? method just tells us if an element is contained in a seq"
           (it "Should return true when an element is contained inside a seq"
-              (should (in? 1 '(1 2 3)))))
+              (should (in? 1 '(1 2 3))))
+          (it "Should return false when an element is not contained inside a seq"
+              (should-not (in? 4 '(1 2 3)))))
+
+(describe "Is-prime checks if an element is a prime number"
+          (it "Should return true when a number is prime"
+              (should (is-prime 3)))
+          (it "Should return false when a number is not prime"
+              (should-not (is-prime 4))))
